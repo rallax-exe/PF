1f3293f1f833445768a640e41fb83fff9e9faac7
rallax-hack <raul.lara.unam@gmail.com>
2020-06-16 21:34:52 -0500

    Act

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,26 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Juguetes' });
 });
 
+router.post('/alta',function(req,res,next){
+	var mjuguete=Juguete({
+		nombre:req.body.nombre,
+    material:req.body.material,
+    tamanio:req.body.tamanio,
+    modelo:req.body.modelo,
+    pilas:req.body.pilas,
+    compania:req.body.compania,
+		foto:req.body.foto,
+    descripcion:req.body.descripcion
+	});
+
+	mjuguete.save(function(err,data){
+		if (err) {
+			console.log('error');
+		}else{
+			res.render('resultadoAlta',data);
+		}
+	});
+});
 
 
 module.exports = router;
